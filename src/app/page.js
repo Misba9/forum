da77ffec28254041616d252f79e884562828b1c7
Mohammed Zaker <iamzaker786@gmail.com>
2024-04-27 14:43:24 +0530

    changed background

--- src/app/page.js
+++ src/app/page.js
@@ -2,35 +2,35 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="h-screen flex items-center justify-center bg-slate-50">
-      <div className="bg-white p-6 rounded-lg w-[80%] max-w-md shadow-lg">
-        <h3 className="text-black font-bold text-xl mb-4 text-center">
-          Sign in to your account
+      <div className="p-6 rounded-lg w-[80%] max-w-md">
+        <h3 className="text-black font-bold text-xl mb-4 text-center font-serif">
+          Sign in
         </h3>
         <form>
           <div className="mb-4">
-            <label htmlFor="email" className="block text-sm font-bold mb-1">
+            <label htmlFor="email" className="block text-sm font-serif mb-1">
               Email
             </label>
             <input
               type="email"
               id="email"
               placeholder="Enter your email"
-              className="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:border-indigo-500"
+              className="w-full px-3 py-2 border font-serif border-black rounded focus:outline-none focus:border-indigo-500"
             />
           </div>
           <div className="mb-4 relative">
-            <label htmlFor="password" className="block text-sm font-bold mb-1">
+            <label htmlFor="password" className="block text-sm font-serif mb-1">
               Password
             </label>
             <input
               type="password"
               id="password"
               placeholder="Enter your password"
-              className="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:border-indigo-500"
+              className="w-full px-3 py-2 border font-serif border-black rounded focus:outline-none focus:border-indigo-500"
             />
             <div className="absolute top-0 right-0 mb-2">
               <Link href="/forgot">
-              <span className="text-sm font-bold text-indigo-600 hover:text-indigo-400 cursor-pointer">
+              <span className="text-sm font-serif text-indigo-600 hover:text-indigo-400 cursor-pointer">
                 Forgot password?
               </span>
               </Link>
@@ -38,18 +38,18 @@ export default function Home() {
           </div>
           <button
             type="submit"
-            className="w-full bg-indigo-600 text-white py-2 rounded-md hover:bg-indigo-700 transition duration-300"
+            className="w-full bg-indigo-600 text-white py-2 rounded-md hover:bg-indigo-700 transition duration-300 font-serif"
           >
             Sign In
           </button>
         </form>
         <div
-        className="text-center mt-9"
+        className="text-center mt-6"
         >
-         <span className="text-black">Not a member? </span>
+         <span className="text-black font-serif">Not a member? </span>
          <Link href="/signup">
           <span
-          className="text-indigo-600 hover:text-indigo-400 font-semibold cursor-pointer"
+          className="text-indigo-600 hover:text-indigo-400 cursor-pointer font-serif"
           > Sign Up Here
           </span>
          </Link>
--- src/app/signup/page.js
+++ src/app/signup/page.js
@@ -2,55 +2,55 @@ import Link from "next/link";
 export default function Signup() {
   return (
     <main className="h-screen flex items-center justify-center bg-slate-50">
-      <div className="bg-white p-6 rounded-lg w-[80%] max-w-md shadow-lg">
-        <h3 className="text-black font-bold text-xl mb-4 text-center">
+      <div className="p-6 rounded-lg w-[80%] max-w-md">
+        <h3 className="text-black font-bold text-xl mb-4 text-center font-serif">
           Sign Up
         </h3>
         <form>
           <div className="mb-4">
-            <label htmlFor="email" className="block text-sm font-bold mb-1">
+            <label htmlFor="email" className="block text-sm font-serif mb-1">
               Email
             </label>
             <input
               type="email"
               id="email"
               placeholder="Enter your email"
-              className="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:border-indigo-500"
+              className="w-full px-3 py-2 border border-black rounded focus:outline-none font-serif focus:border-indigo-500"
             />
           </div>
           <div className="mb-4 relative">
-            <label htmlFor="password" className="block text-sm font-bold mb-1">
+            <label htmlFor="password" className="block text-sm font-serif mb-1">
               Password
             </label>
             <input
               type="password"
               id="password"
               placeholder="Enter your password"
-              className="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:border-indigo-500"
+              className="w-full px-3 py-2 border font-serif border-black rounded focus:outline-none focus:border-indigo-500"
             />
           </div>
           <div className="mb-4 relative">
-            <label htmlFor="password-2" className="block text-sm font-bold mb-1">
+            <label htmlFor="password-2" className="block text-sm font-serif mb-1">
               Password
             </label>
             <input
               type="password"
               id="password-2"
               placeholder="Confirm password"
-              className="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:border-indigo-500"
+              className="w-full px-3 py-2 border font-serif border-black rounded focus:outline-none focus:border-indigo-500"
             />
           </div>
           <button
             type="submit"
-            className="w-full bg-indigo-600 text-white py-2 rounded-md hover:bg-indigo-700 transition duration-300"
+            className="w-full font-serif bg-indigo-600 text-white py-2 rounded-md hover:bg-indigo-700 transition duration-300"
           >
             Sign Up
           </button>
         </form>
-        <div className="text-center mt-9">
-          <span className="text-black">Already a member? </span>
+        <div className="text-center mt-6">
+          <span className="text-black font-serif">Already a member? </span>
           <Link href="/">
-            <span className="text-indigo-600 hover:text-indigo-400 font-semibold cursor-pointer">
+            <span className="text-indigo-600 hover:text-indigo-400 font-serif cursor-pointer">
               Sign in Here
             </span>
           </Link>
